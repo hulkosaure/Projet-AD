7ec9f7db4782d4728b833a42f0a9b3ed3beec1aa
adrien raimbault <adrien.raimbault@etu.univ-lyon1.fr>
2023-11-20 17:10:36 +0100

    fin de séance

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import plotly.express as px
 import pandas as pd
 
 
@@ -10,9 +11,21 @@ app = Dash(__name__)
 
 # App layout
 app.layout = html.Div([
-    html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    html.Div(children='My First App with Data, Graph, and Controls'),
+    html.Hr(),
+    dcc.RadioItems(options=['liveness', 'explicit', 'danceability'], value='explicit', id='controls-and-radio-item'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    dcc.Graph(figure={}, id='controls-and-graph')
 ])
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='year', y=col_chosen, histfunc='avg')
+    return fig
 
 # Run the app
 if __name__ == '__main__':
