37762898644cf95be31fc2519219e6f49bac7134
Pierre Gerenton <pierre.gerenton.etu@univ-lille.fr>
2023-12-15 11:22:49 +0100

    new data

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 
 # Incorporate data
-songs = pd.read_csv('songs.csv')
-artists = pd.read_csv('artists.csv')
+full_dataset = pd.read_csv('full_dataset.csv')
+
+
 
 # Initialize the app
 app = Dash(__name__)
@@ -14,19 +15,17 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div(children='My First App with Data, Graph, and Controls'),
     html.Hr(),
-    dcc.RadioItems(options=['liveness', 'explicit', 'danceability'], value='explicit', id='controls-and-radio-item'),
-    dash_table.DataTable(data=songs.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='controls-and-graph')
+    dash_table.DataTable(data=full_dataset.to_dict("records"), page_size=300),
 ])
 
 # Add controls to build the interaction
-@callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(songs, x='year', y=col_chosen, histfunc='avg')
-    return fig
+# @callback(
+#     Output(component_id='controls-and-graph', component_property='figure'),
+#     Input(component_id='controls-and-radio-item', component_property='value')
+# )
+# def update_graph(col_chosen):
+#     fig = px.histogram(songs, x='year', y=col_chosen, histfunc='avg')
+#     return fig
 
 # Run the app
 if __name__ == '__main__':
