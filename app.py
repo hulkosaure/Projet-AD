4083a50321323b8919352ff5fe0ea1c6b82dd360
Pierre Gerenton <pierre.gerenton.etu@univ-lille.fr>
2023-12-16 23:05:01 +0100

    acp reparer

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input, State, ctx
 import dash_ag_grid as dag
 import dash_daq as daq
-
 import plotly.express as px
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -12,6 +11,9 @@ from sklearn.decomposition import PCA
 # Incorporate data
 full_dataset = pd.read_csv('data/full_dataset.csv')
 columns = full_dataset.columns.tolist()
+numeric_columns = columns.copy()
+numeric_columns.remove("country")
+numeric_columns.remove("region")
 
 # Clean the data
 def convertir_number(x):
@@ -38,12 +40,11 @@ app.layout = html.Div(
             dcc.Tab(label="Analyse", value="t2", children=[
                 html.Label('Sélectionnez les colonnes à afficher :'),
                 dcc.Dropdown(
-                id='column-dropdown',
-                options=[{'label': col, 'value': col} for col in full_dataset.columns],
-                multi=True,
-                value=full_dataset.columns,
+                    id='column-dropdown',
+                    options=[{'label': col, 'value': col} for col in numeric_columns],
+                    multi=True,
+                    value=numeric_columns,
                 ),
-                html.Button('Mettre à jour', id='update-button', n_clicks=0), 
                 html.Button('Réinitialiser', id='reset-button', n_clicks=0),
                 dag.AgGrid(
                     id="test",
@@ -87,8 +88,9 @@ app.layout = html.Div(
                         html.Label('Ceci est un test :'),
                         dcc.Dropdown(
                             id='analysed_data',
-                            options=[{'label': col, 'value': col} for col in full_dataset.columns],
+                            options=[{'label': col, 'value': col} for col in numeric_columns],
                             multi=True,
+                            value=numeric_columns
                             ),
                         dcc.RadioItems(
                             id = "clusteringmethod",
@@ -105,11 +107,11 @@ app.layout = html.Div(
                             daq.NumericInput(id="min_samples",
                                 value = 5,
                                 size = 60,
-                                min = 0,
+                                min = 1,
                                 max = 50,
                                 label = "Nombre minumum de point par cluster (int)")
                         ]),
-                        dcc.Graph(id="clusterplot"), 
+                        dcc.Graph(id="clusterplot"),
                         dcc.Graph(id="clusterplot3d"),
                     ])
                 ]),
@@ -124,18 +126,16 @@ app.layout = html.Div(
     Output('test', 'columnDefs'),
     Output('test', 'rowData'),
     Output('column-dropdown', 'value'),
-    Input('update-button', 'n_clicks'),
     Input('reset-button', 'n_clicks'),
-    State('column-dropdown', 'value')
+    Input('column-dropdown', 'value')
 )
 
-
-def update_data(update_clicks, reset_clicks, selected_columns):
-    changed_id = [p['prop_id'] for p in ctx.triggered][0]
+def update_data(reset_clicks, selected_columns):
     
     # Ajout des colonnes 'country' et 'region' en premier dans la sélection
     selected_columns = list(set(selected_columns + ['country', 'region']))
     
+    changed_id = [p['prop_id'] for p in ctx.triggered][0]
     if 'reset-button' in changed_id and reset_clicks > 0:
         updated_column_defs = [{"field": i} for i in full_dataset.columns]
         updated_data = full_dataset.to_dict('records')
@@ -149,14 +149,11 @@ def update_data(update_clicks, reset_clicks, selected_columns):
     
     return updated_column_defs, updated_data, dropdown_value
 
-
 @callback(
     Output("scatterplot", "figure"),
     Input("plot_x", "value"),
     Input("plot_y", "value")
 )
-
-
 def scatter(plot_x, plot_y):
     if plot_x and plot_y: 
         fig = px.scatter(
@@ -171,40 +168,53 @@ def scatter(plot_x, plot_y):
         return {}
 
 
+
+
 @callback(
     Output("clusterplot", "figure"),
-    Output("clusterplot3d", "figure"),
-    Input("clusteringmethod", "value"),
-    Input("test", "rowData"),
-    State("analysed_data", "value")
+    Output("clusterplot3d","figure"),
+    Input("clusteringmethod","value"),
+    Input("esp","value"),
+    Input("min_samples","value"),
+    Input("analysed_data", "value")
 )
-def cluster_and_represent(cmethod, data, selected_columns):
-    data = pd.DataFrame.from_dict(data).dropna()
-    data_selected = data[selected_columns]
-    
+def cluster_and_represent(cmethod, esp, min_samples, selected_columns):
+
+    # Select wanted data
+    selected_columns_extended = list(set(selected_columns + ['country', 'region']))
+    data_selected = full_dataset[selected_columns_extended].dropna()
+
+    # Get country and region vector for hover data later
+    country = data_selected["country"]
+    region = data_selected["region"]
+
+    # Get only numerical data
+    data_selected = data_selected[selected_columns]
+
+    # Normalize
     scaler = StandardScaler()
     data_scaler = scaler.fit_transform(data_selected)
-    
-    clusterer = DBSCAN() if cmethod == "DBSCAN" else AffinityPropagation()
+
+    # Clustering
+    clusterer = DBSCAN(eps = esp, min_samples = min_samples) if cmethod=="DBSCAN" else AffinityPropagation()
     clustering = clusterer.fit(data_scaler)
-    
+
+    # PCA
     pca = PCA(n_components=2)
     pca3d = PCA(n_components=3)
     data_pca = pca.fit_transform(data_scaler)
     data_pca_3d = pca3d.fit_transform(data_scaler)
-    
-    fig = px.scatter(
-        data_pca,
+
+    # Plots
+    fig = px.scatter(data_pca, x=0, y=1,
         color=clustering.labels_,
-        hover_data=[data["country"], data["region"]]
+        hover_data=[country,region]
     )
-    
     fig3d = px.scatter_3d(
         data_pca_3d, x=0, y=1, z=2,
         color=clustering.labels_,
-        hover_data=[data["country"], data["region"]]
+        hover_data=[country,region]
     )
-    
     return fig, fig3d
 
 
@@ -212,14 +222,6 @@ def cluster_and_represent(cmethod, data, selected_columns):
     Output("dbscan_parameters","style"),
     Input("clusteringmethod","value")
 )
-
-
-def display_dbscan_param(clusteringmethod):
-    """
-    Afficher le choix des paramètres pour DBSCAN si DBSCAN est la méthode choisit.
-    """
-
-
 def display_dbscan_param(clusteringmethod):
     """
     Afficher le choix des paramètres pour DBSCAN si DBSCAN est la méthode choisit.
@@ -233,8 +235,6 @@ def display_dbscan_param(clusteringmethod):
     Output("min_samples","value"),
     Input("min_samples","value")
 )
-
-
 def check_int_value(val):
     """
     Vérifie si la valeur mise pour min_samples (DBSCAN) est entière.
